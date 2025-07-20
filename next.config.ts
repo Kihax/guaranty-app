cc642fea1feecf013b188414c3fa44543a8dd79c
KERLAU <killian.kerlau@imt-atlantique.net>
2025-07-20 15:15:40 +0200

    edit csf policy

--- next.config.ts
+++ next.config.ts
@@ -1,23 +1,23 @@
 import type { NextConfig } from "next";
 
-const nextConfig = {
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: `
+      script-src 'self' 'unsafe-inline' https://apis.google.com https://www.gstatic.com https://accounts.google.com;
+      frame-src https://accounts.google.com;
+      connect-src 'self' https://accounts.google.com https://www.googleapis.com;
+      img-src 'self' data: https://*.googleusercontent.com;
+    `.replace(/\s{2,}/g, " ").trim()
+  },
+];
+
+module.exports = {
   async headers() {
     return [
       {
-        source: '/(.*)', // toutes les routes
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: `
-              default-src 'self';
-              script-src 'self' https://apis.google.com https://accounts.google.com https://www.gstatic.com 'unsafe-inline';
-              connect-src 'self' https://accounts.google.com https://www.googleapis.com;
-              frame-src https://accounts.google.com;
-              style-src 'self' 'unsafe-inline';
-              img-src 'self' data: https://*.googleusercontent.com;
-            `.replace(/\s{2,}/g, ' ').trim(),
-          },
-        ],
+        source: "/(.*)",
+        headers: securityHeaders,
       },
     ];
   },
