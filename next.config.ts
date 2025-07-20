cf39fbbf8ab104f1475dae76a2f3efe717509962
KERLAU <killian.kerlau@imt-atlantique.net>
2025-07-20 15:50:49 +0200

    edit csf

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,13 @@ import type { NextConfig } from "next";
 
 const ContentSecurityPolicy = `
   default-src 'self';
-  script-src 'self' 'unsafe-inline' https://apis.google.com https://accounts.google.com https://www.gstatic.com;
+  script-src 'self' 'unsafe-inline' 'unsafe-eval' https://apis.google.com https://accounts.google.com https://www.gstatic.com;
   style-src 'self' 'unsafe-inline';
   img-src 'self' data: https:;
-  connect-src 'self' https://www.googleapis.com;
-  frame-src https://accounts.google.com;
+  connect-src 'self' https://www.googleapis.com https://accounts.google.com https://content.googleapis.com;
+  frame-src https://accounts.google.com https://content.googleapis.com;
+  frame-ancestors 'self';
+  object-src 'none';
 `;
 
 const nextConfig = {
