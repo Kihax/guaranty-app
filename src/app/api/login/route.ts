a8f87ca1d061dc48df9f5f44162b27417e709ed0
KERLAU <killian.kerlau@imt-atlantique.net>
2025-07-21 20:58:43 +0200

    Updated to last app router on api/login

---
+++ src/app/api/login/route.ts
@@ -0,0 +1,39 @@
+import { NextResponse } from 'next/server'
+import { cookies } from 'next/headers'
+
+export async function POST(request: Request) {
+  const body = await request.json()
+  const { token } = body
+
+  if (!token) {
+    return NextResponse.json({ message: 'Token is missing' }, { status: 400 })
+  }
+
+  try {
+    const userResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/user`, {
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+
+    if (!userResponse.ok) {
+      throw new Error('Failed to fetch user')
+    }
+
+    const user = await userResponse.json()
+
+    const cookieStore = await cookies();
+    cookieStore.set('token', token);
+    cookieStore.set('id', user.id);
+    cookieStore.set('email', user.email);
+    cookieStore.set('fullName', user.fullName);
+    cookieStore.set('emailVerified', user.emailVerified);
+
+
+    return NextResponse.json({ message: 'Login successful' })
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : 'An unknown error occurred';
+    return NextResponse.json({ message }, { status: 500 })
+  }
+}
