8e9a5b390d4b51cae070bc5a22718ac8c8ca16bb
KERLAU <killian.kerlau@imt-atlantique.net>
2025-07-22 19:52:14 +0200

    api logout router wrong module

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ import { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import GoogleButton from "../lib/GoogleButton";
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 
 
 export default function Login() {
