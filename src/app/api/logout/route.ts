1ca14433266d97e07d2c70151efe40c091876464
KERLAU <killian.kerlau@imt-atlantique.net>
2025-07-22 22:18:53 +0200

    api logout redirect to login

--- src/app/api/logout/route.ts
+++ src/app/api/logout/route.ts
@@ -1,48 +1,54 @@
-import { NextResponse } from 'next/server'
-import { cookies } from 'next/headers'
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+import { redirect } from 'next/navigation'
 
 export async function GET(request: Request) {
+	const cookieStore = await cookies();
 
-    const cookieStore = await cookies();
+	const token = cookieStore.get("token")?.value;
 
-  const token = (await cookies()).get("token");
+	try {
+		const userResponse = await fetch(
+			`${process.env.NEXT_PUBLIC_API_URL}/auth/user`,
+			{
+				method: "GET",
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			}
+		);
 
-  console.log("Token in logout:", token);
+		console.log("User response:", userResponse);
 
-  try {
-    const userResponse = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/user`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
+		if (!userResponse.ok) {
+			throw new Error("Failed to fetch user");
+		}
 
-    console.log('User response:', userResponse);
+		const user = await userResponse.json();
 
-    if (!userResponse.ok) {
-      throw new Error('Failed to fetch user')
-    }
+		try {
+			fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
+				method: "POST",
+				headers: {
+					Authorization: `Bearer ${token}`,
+				},
+			});
+		} catch (error) {
+			console.error("Error during logout:", error);
+		}
+		cookieStore.delete("token");
+        cookieStore.delete("id");
+        cookieStore.delete("email");
+        cookieStore.delete("fullName");
+        cookieStore.delete("emailVerified");
+        redirect('/login'); // Redirect to login page after logout
 
-    const user = await userResponse.json()
-
-    try {
-        fetch(`${process.env.NEXT_PUBLIC_API_URL}/auth/logout`, {
-          method: 'POST',
-            headers: {
-                Authorization: `Bearer ${token}`,
-            },
-        });
-    } catch (error) {
-        console.error('Error during logout:', error);
-    }
-    const cookieStore = await cookies();
-    console.log('Cookie store before deletion:', cookieStore.getAll());
-    cookieStore.delete('guaranty_session');
-
-
-    return NextResponse.json({ message: 'Logout successful' })
-  } catch (error: unknown) {
-    const message = error instanceof Error ? error.message : 'An unknown error occurred';
-    return NextResponse.json({ message }, { status: 500 })
-  }
+		return NextResponse.json({ message: "Logout successful" });
+	} catch (error: unknown) {
+		const message =
+			error instanceof Error
+				? error.message
+				: "An unknown error occurred";
+		return NextResponse.json({ message }, { status: 500 });
+	}
 }
