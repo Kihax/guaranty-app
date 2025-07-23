cd27314e8e141ce247520ce0620c80ed1fe04067
KERLAU <killian.kerlau@imt-atlantique.net>
2025-07-23 20:40:35 +0200

    load token

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,17 @@
 import "../globals.css";
+import { cookies } from 'next/headers'
 
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+  const cookieStore = await cookies()
+  const token = cookieStore.get("token")?.value;
+
+  console.log("Token in Dashboard:", token);
+
+  if (!token) {
+    
+  }
+
   return (
     <div>
       <h1>Dashboard</h1>
