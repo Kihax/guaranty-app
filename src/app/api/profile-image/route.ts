2397502877fe916eda9037bc37d5eb842675b068
KERLAU <killian.kerlau@imt-atlantique.net>
2025-08-02 19:17:19 +0200

    update url

--- src/app/api/profile-image/route.ts
+++ src/app/api/profile-image/route.ts
@@ -11,7 +11,7 @@ export async function GET() {
 
 	try {
 		const response = await fetch(
-			`${process.env.NEXT_PUBLIC_API_URL}/auth/profile-image`,
+			`${process.env.NEXT_PUBLIC_API_URL}/auth/get-profile-picture`,
 			{
 				method: "GET",
 				headers: {
