4b5a18b8b79c1f7067954489ad113d80c8a8541e
KERLAU <killian.kerlau@imt-atlantique.net>
2025-08-09 13:31:07 +0200

    add background to add button

--- src/components/AddButtonDashboard.tsx
+++ src/components/AddButtonDashboard.tsx
@@ -1,6 +1,6 @@
 export default function AddButtonDashboard() {
 	return (
-		<div className="fixed bottom-0 right-0 z-10 py-4 px-6 flex justify-end">
+		<div className="fixed bottom-0 right-0 z-10 py-4 px-6 flex justify-end bg-white/50 dark:bg-gray-900/60 backdrop-blur-md">
 			<button
 				type="button"
 				onClick={() => (window.location.href = "/dashboard/add")}
